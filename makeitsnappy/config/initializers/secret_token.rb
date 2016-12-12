7c1d37f0a3cc7d4667883d8927c72d6cfd5d4695
Rares Dragus <dev.rares.dragus@gmail.com>
2016-12-12 22:16:33 +0200

    Creates make it snappy project

---
+++ makeitsnappy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makeitsnappy::Application.config.secret_token = 'ff48a661836ff981b7f4d2692a50a76eb26f36593c91779712e67297172e358cd37da17f12c9cfc639ab7ed03f0bf779345ab605025083ef9bdc98a791fea7a8'
